e5482dfdd198c9ebd2ff10564515b1c65eb0b9f6
MuhammadRezaHeidary <muhammadreza.heidary@gmail.com>
2023-04-26 23:36:24 +0430

    Creating project!

---
+++ mekatron-banner.php
@@ -0,0 +1,38 @@
+<?php
+
+/**
+Plugin Name: Mekatron Banner
+Plugin URI: https://github.com/MuhammadRezaHeidary/mekatron-banner
+Description: This plugin shows a banner on your webpage!
+Version: 1.0.0
+Requires at least: 5.6
+Requires PHP:      7.4
+Author: Muhammmad Reza Heidary
+Author URI: https://www.linkedin.com/in/muhammad-reza-heidary/
+License: GPL v2 or later
+License URI: https://www.gnu.org/licenses/gpl-3.0.en.html
+Update URI: https://github.com/MuhammadRezaHeidary/mekatron-banner
+ */
+
+/*
+mekatron-banner is a free Wordpress plugin which shows a banner on your webpage.
+Copyright (C)  2023  Muhammad Reza Heidary
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+mekatron-banner Copyright (C)  2023  Muhammad Reza Heidary
+This program comes with ABSOLUTELY NO WARRANTY.
+This is free software, and you are welcome to redistribute it
+under certain conditions.
+ */
